0d6e7726c2f606d95e06889ad2848944869392d7
Shek Khaja Mohinuddin <shaikkhajade@gmail.com>
2026-02-24 13:24:21 +0530

    Spark SQL practice

---
+++ first.py
@@ -0,0 +1,123 @@
+import os
+import urllib.request
+import ssl
+
+from pyspark.sql.utils import capture_sql_exception
+
+data_dir = "data"
+os.makedirs(data_dir, exist_ok=True)
+
+data_dir1 = "hadoop/bin"
+os.makedirs(data_dir1, exist_ok=True)
+
+hadoop_home = os.path.abspath("hadoop")   # <-- absolute path
+os.makedirs(os.path.join(hadoop_home, "bin"), exist_ok=True)
+
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.12:3.2.0 pyspark-shell'
+# ======================================================================================
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import sys
+import os
+import urllib.request
+import ssl
+
+python_path = sys.executable
+os.environ['PYSPARK_PYTHON'] = python_path
+os.environ['HADOOP_HOME'] = hadoop_home
+os.environ['JAVA_HOME'] = r'C:\Users\hamza\.jdks\corretto-1.8.0_472'        #  <----- 🔴JAVA PATH🔴
+######################🔴🔴🔴################################
+
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.12:3.5.1 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-avro_2.12:3.5.4 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.4 pyspark-shell'
+
+
+conf = SparkConf().setAppName("pyspark").setMaster("local[*]").set("spark.driver.host","localhost").set("spark.default.parallelism", "1")
+sc = SparkContext(conf=conf)
+
+spark = SparkSession.builder.getOrCreate()
+
+spark.read.format("csv").load("data/test.txt").toDF("Success").show(20, False)
+
+
+##################🔴🔴🔴🔴🔴🔴 -> DONT TOUCH ABOVE CODE -- TYPE BELOW ####################################
+
+# SQL GET READY CODE
+
+
+data = [
+    (0, "06-26-2011", 300.4, "Exercise", "GymnasticsPro", "cash"),
+    (1, "05-26-2011", 200.0, "Exercise Band", "Weightlifting", "credit"),
+    (2, "06-01-2011", 300.4, "Exercise", "Gymnastics Pro", "cash"),
+    (3, "06-05-2011", 100.0, "Gymnastics", "Rings", "credit"),
+    (4, "12-17-2011", 300.0, "Team Sports", "Field", "cash"),
+    (5, "02-14-2011", 200.0, "Gymnastics", None, "cash"),
+    (6, "06-05-2011", 100.0, "Exercise", "Rings", "credit"),
+    (7, "12-17-2011", 300.0, "Team Sports", "Field", "cash"),
+    (8, "02-14-2011", 200.0, "Gymnastics", None, "cash")
+]
+
+df = spark.createDataFrame(data, ["id", "tdate", "amount", "category", "product", "spendby"])
+df.show()
+
+
+
+
+
+data2 = [
+    (4, "12-17-2011", 300.0, "Team Sports", "Field", "cash"),
+    (5, "02-14-2011", 200.0, "Gymnastics", None, "cash"),
+    (6, "02-14-2011", 200.0, "Winter", None, "cash"),
+    (7, "02-14-2011", 200.0, "Winter", None, "cash")
+]
+
+df1 = spark.createDataFrame(data2, ["id", "tdate", "amount", "category", "product", "spendby"])
+df1.show()
+
+
+
+
+
+
+data4 = [
+    (1, "raj"),
+    (2, "ravi"),
+    (3, "sai"),
+    (5, "rani")
+]
+
+
+
+cust = spark.createDataFrame(data4, ["id", "name"])
+cust.show()
+
+data3 = [
+    (1, "mouse"),
+    (3, "mobile"),
+    (7, "laptop")
+]
+
+prod = spark.createDataFrame(data3, ["id", "product"])
+prod.show()
+
+
+df.createOrReplaceTempView("df")
+df1.createOrReplaceTempView("df1")
+cust.createOrReplaceTempView("cust")
+prod.createOrReplaceTempView("prod")
+
+
+
+spark.sql ("select * , case when  spendby = 'cash' then 1 else '0' end as status from df").show()
+
+
+spark.sql("select * , case when spendby = 'cash' then 1 when spendby = 'credit' then 2 else 0 end as status from df").show()
+
+
+spark.sql("select id,category, concat(id,'-',category)as condata from df ").show()
+
+spark.sql("select id,category,product, concat_ws('-',id,category,product) as condata from df ").show()
+
